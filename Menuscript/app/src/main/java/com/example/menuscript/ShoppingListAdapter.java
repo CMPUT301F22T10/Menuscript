ad1285e1a5c2ec8fa40ad33ebdba758b5dbda7e6
Joshua Vuong <jtvuong@ualberta.ca>
2022-11-21 20:44:00 -0700

    Shopping list adapter changes

---
+++ Menuscript/app/src/main/java/com/example/menuscript/ShoppingListAdapter.java
@@ -0,0 +1,35 @@
+package com.example.menuscript;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class ShoppingListAdapter extends ArrayAdapter<Ingredient> {
+    private Context context;
+    private ArrayList<Ingredient> mealPlanIngredients;
+
+    public ShoppingListAdapter(Context context, ArrayList<Ingredient> mealPlanIngredients) {
+        super(context, 0, mealPlanIngredients);
+        this.context = context;
+        this.mealPlanIngredients = mealPlanIngredients;
+    }
+
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.shoplist_maincontent, parent, false);
+        }
+        Ingredient ingredient = mealPlanIngredients.get(position);
+
+        return view;
+    }
+}
