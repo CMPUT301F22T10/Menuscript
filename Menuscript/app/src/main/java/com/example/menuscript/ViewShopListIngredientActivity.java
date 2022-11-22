455e9b848c0bd84fd2a9d6de772eed4895ce583f
Joshua Vuong <jtvuong@ualberta.ca>
2022-11-22 15:36:15 -0700

    Revert "Changes values displayed to proper values"
    This reverts commit 0451f18cb331fd62c35f6cfad5a4568a95a5ac96.

--- Menuscript/app/src/main/java/com/example/menuscript/ViewShopListIngredientActivity.java
+++ Menuscript/app/src/main/java/com/example/menuscript/ViewShopListIngredientActivity.java
@@ -6,17 +6,7 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
 
 public class ViewShopListIngredientActivity extends AppCompatActivity {
 
@@ -24,7 +14,6 @@ public class ViewShopListIngredientActivity extends AppCompatActivity {
     private EditText ingredientCount;
     private EditText ingredientUnit;
     private EditText ingredientCategory;
-    private StoredIngredient selectedIngredient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +27,12 @@ public class ViewShopListIngredientActivity extends AppCompatActivity {
         ingredientUnit = findViewById(R.id.unitEditText);
         ingredientCategory = findViewById(R.id.categoryEditText);
 
-        ingredientName.setText(bundle.getString("NAME"));
+        if (bundle.getString("NAME")!= null) {
+            ingredientName.setText(bundle.getString("NAME"));
+        }
         ingredientCount.setText(Float.toString(bundle.getFloat("AMOUNT")));
         ingredientUnit.setText(bundle.getString("CATEGORY"));
         ingredientCategory.setText(bundle.getString("UNIT"));
+
     }
 }
